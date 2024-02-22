762838d815a141db081264b2954bad8ab7a8071e
Popke <popek069@gmail.com>
2024-02-22 22:09:20 +0100

    Dodaj pliki projektów.

---
+++ Crew.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjectOrientedDesignProject
+{
+    internal class Crew
+    {
+        int ID;
+        string Name;
+        UInt64 Age;
+        string Phone;
+        string Email;
+        UInt16 Practice;
+        string Role;
+    }
+}
---
+++ Passenger.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjectOrientedDesignProject
+{
+    internal class Passenger
+    {
+        UInt64 ID;
+        string Name;
+        UInt64 Age;
+        string Phone;
+        string Email;
+        string Class;
+        UInt64 Miles;
+    }
+}
