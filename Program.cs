a8d725ffa59c2a005583df7e31440b686f8385a8
Popke <popek069@gmail.com>
2024-02-26 22:16:26 +0100

    Add importing and serializing data in the main function

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ObjectOrientedDesign
+﻿using System.Text.Json;
+
+namespace ObjectOrientedDesign
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<IImport> imported = new List<IImport>();
+
+            Dictionary<string, IFactory> keyValuePairs = new Dictionary<string, IFactory>() {
+                {"C", new CrewFactory() },
+                {"P", new PassengerFactory() },
+                {"CA", new CargoFactory() },
+                {"CP", new CargoPlaneFactory() },
+                {"PP", new PassengerPlaneFactory() },
+                {"AI", new AirportFactory() },
+                {"FL", new FlightFactory() }
+            };
+
+            using (StreamReader sr = new StreamReader("example_data.ftr"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] split = line.Split(',');
+                    imported.Add(keyValuePairs[split[0]].CreateFromString(line));
+                }
+            }
+
+            string fileName = "serialized.json";
+            string jsonString = JsonSerializer.Serialize(imported);
+            File.WriteAllText(fileName, jsonString);
         }
     }
 }
