fd98d00199dc7dca2a6c26ee1a7a06fa0dcf0ba3
Popke <popek069@gmail.com>
2024-02-26 22:09:46 +0100

    Add IFactory and IImport interfaces

---
+++ IFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace ObjectOrientedDesign
+{
+    public interface IFactory
+    {
+        public IImport CreateFromString(string s);
+    }
+}
---
+++ IImport.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ObjectOrientedDesign
+{
+    [JsonDerivedType(typeof(Airport), "AI")]
+    [JsonDerivedType(typeof(Cargo), "CA")]
+    [JsonDerivedType(typeof(CargoPlane), "CP")]
+    [JsonDerivedType(typeof(Crew), "C")]
+    [JsonDerivedType(typeof(Flight), "FL")]
+    [JsonDerivedType(typeof(Passenger), "P")]
+    [JsonDerivedType(typeof(PassengerPlane), "PP")]
+    public interface IImport
+    {
+
+    }
+}
