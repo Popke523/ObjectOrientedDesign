2ebf50070be25011872c2775416a4aa658540b56
Popke <popek069@gmail.com>
2024-03-13 08:55:52 +0100

    out of date comment

--- FlightSystem/UnixTimeStamp.cs
+++ FlightSystem/UnixTimeStamp.cs
@@ -2,7 +2,6 @@ namespace ObjectOrientedDesign.FlightSystem;
 
 public class UnixTimeStamp
 {
-    // https://stackoverflow.com/questions/249760/how-can-i-convert-a-unix-timestamp-to-datetime-and-vice-versa
     public static DateTime ToDateTime(long unixTimeStamp)
     {
         // Unix timestamp is seconds past epoch
